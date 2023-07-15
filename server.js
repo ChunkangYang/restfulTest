1647eeb763827668c6ceaf0a055289daa2fe7ee4
Stan <cky1983@gmail.com>
2023-07-15 15:10:02 +0900

    t

--- server.js
+++ server.js
@@ -16,9 +16,17 @@ app.post('/recipes', async (req, res) => {
     const query = 'INSERT INTO recipes (title, making_time, serves, ingredients, cost) VALUES ($1, $2, $3, $4, $5) RETURNING *';
     const values = [title, making_time, serves, ingredients, cost];
     const result = await pool.query(query, values);
-    res.status(201).json(result.rows[0]);
+    const resQuery = 'SELECT * FROM recipes';
+    const resResult = await pool.query(resQuery);
+    const recipes = resResult.rows.map(row => ({ id: row.id, title: row.title, making_time: row.making_time,
+        servers: row.servers, ingredients: row.ingredients, cost: row.cost, created_at: row.created_at, updated_at: row.updated_at }));
+
+    res.status(200).json({
+        message: '',
+        recipe: result.rows[0]
+    })
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ message: 'Recipe creation failed!', required: 'title, making_time, serves, ingredients, cost' });
   }
 });
 
@@ -29,11 +37,7 @@ app.get('/recipes', async (req, res) => {
     const result = await pool.query(query);
     const recipes = result.rows.map(row => ({ id: row.id, title: row.title, making_time: row.making_time,
         servers: row.servers, ingredients: row.ingredients, cost: row.cost }));
-    res.json({ recipes });
-    // const query = 'SELECT * FROM recipes';
-    // const result = await pool.query(query);
-    // const recipes = result.rows.map(row => ({ id: row.id }));
-    // res.json({ recipes });
+    res.status(200).json({ recipes });
   } catch (err) {
     res.status(500).json({ error: 'Internal server error', e: err });
   }
