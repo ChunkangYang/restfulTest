33c3336a70b980a9653abd7281c711b86b438cc1
Stan <cky1983@gmail.com>
2023-07-15 14:42:37 +0900

    t

--- server.js
+++ server.js
@@ -25,9 +25,10 @@ app.post('/recipes', async (req, res) => {
 
 app.get('/recipes', async (req, res) => {
   try {
-    const query = 'SELECT id, title FROM recipes';
+    const query = 'SELECT id, title, making_time, servers, ingredients, cost FROM recipes';
     const result = await pool.query(query);
-    const recipes = result.rows.map(row => ({ id: row.id, title: row.title }));
+    const recipes = result.rows.map(row => ({ id: row.id, title: row.title, making_time: row.making_time,
+        servers: row.servers, ingredients: row.ingredients, cost: row.cost }));
     res.json({ recipes });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
